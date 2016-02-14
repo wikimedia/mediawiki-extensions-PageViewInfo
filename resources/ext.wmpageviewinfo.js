c59ad1847385e2dc059009bfa44488e54b8bd263
Kunal Mehta <legoktm@member.fsf.org>
2016-02-14 00:46:06 -0800

    Wait for document ready before adding click handler
    Change-Id: Iffe65887bda60548faf2c4366757ee26b0056799

--- resources/ext.wmpageviewinfo.js
+++ resources/ext.wmpageviewinfo.js
@@ -1,25 +1,28 @@
 ( function( $, mw ) {
-	$( '.mw-wmpvi-month' ).click( function( e ) {
-		// A simple dialog window.
-		function MyDialog( config ) {
-			MyDialog.parent.call( this, config );
-		}
-		OO.inheritClass( MyDialog, OO.ui.Dialog );
-		MyDialog.prototype.initialize = function () {
-			var def = mw.config.get( 'wgWMPageViewInfo' );
-			MyDialog.parent.prototype.initialize.call( this );
-			this.content = new OO.ui.PanelLayout( { padded: true, expanded: false } );
-			this.$body.append( this.content.$element );
-			mw.drawVegaGraph( this.content.$element[0], def );
-		};
-		var myDialog = new MyDialog( {
-			size: 'large'
+	$( function() {
+		$( '.mw-wmpvi-month' ).click( function( e ) {
+			// A simple dialog window.
+			function MyDialog( config ) {
+				MyDialog.parent.call( this, config );
+			}
+			OO.inheritClass( MyDialog, OO.ui.Dialog );
+			MyDialog.prototype.initialize = function () {
+				var def = mw.config.get( 'wgWMPageViewInfo' );
+				MyDialog.parent.prototype.initialize.call( this );
+				this.content = new OO.ui.PanelLayout( { padded: true, expanded: false } );
+				this.$body.append( this.content.$element );
+				mw.drawVegaGraph( this.content.$element[0], def );
+			};
+			var myDialog = new MyDialog( {
+				size: 'large'
+			} );
+			// Create and append a window manager, which opens and closes the window.
+			var windowManager = new OO.ui.WindowManager();
+			$( 'body' ).append( windowManager.$element );
+			windowManager.addWindows( [ myDialog ] );
+			// Open the window!
+			windowManager.openWindow( myDialog );
 		} );
-		// Create and append a window manager, which opens and closes the window.
-		var windowManager = new OO.ui.WindowManager();
-		$( 'body' ).append( windowManager.$element );
-		windowManager.addWindows( [ myDialog ] );
-		// Open the window!
-		windowManager.openWindow( myDialog );
+
 	} );
 }( jQuery, mediaWiki ) );
